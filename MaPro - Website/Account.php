29f3d141f63fd29b239dd303c4048e60062544a7
opsilonn <hugues.begeot@efrei.net>
2019-05-16 16:43:57 +0200

    Redo of the backpage header
    :)

--- MaPro - Website/Account.php
+++ MaPro - Website/Account.php
@@ -10,9 +10,7 @@
     
     // Max length for a row of cards
     $_SESSION['MAX'] = 12;
-
-    $_SESSION['nom'] = "John";
-    $_SESSION['role'] = "Doe";
+    
     
     include "connectdb.php";
 ?>
@@ -72,34 +70,6 @@
     <body>
   <!-- adding the header -->
         <?php include 'headerBackpage.php'; ?>
-
-
-
-  <ul id="slide-out" class="sidenav">
-    <li><div class="user-view">
-      
-      <img class="circle" src="pictures/logo.png">
-      <span class="blueDeep name"> <?php echo $_SESSION['nom'] ?> </span>
-
-      <span class="blueLight email"><?php echo $_SESSION['role'] ?> </span>
-    </div></li>
-    <li><a class="blueLightToBlueDeep" href="account.php"><i class="material-icons left">account_circle</i>My Profile</a></li>
-                <li><a class="blueLightToBlueDeep" href="projectNew.php"><i class="material-icons left">add_to_photos</i>New Project</a></li>
-                <li><a class="blueLightToBlueDeep" href="account.php"><i class="material-icons left">delete_sweep</i>Delete Project</a></li>
-                <li><a class="blueLightToBlueDeep" href="employees.php"><i class="material-icons left">face</i>Employees</a></li>
-				<li><a class="blueLightToBlueDeep" href="teams.php"><i class="material-icons left">groups</i>teams</a></li>
-                <li>
-                    <form>
-                        <div class="input-field">
-                            <input id="search" type="search" required>
-                            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-                            <i class="material-icons">close</i>
-                        </div>
-                    </form>
-                </li>
-                <li><a class=" btn waves-light red lighten-1" href='disconnect.php'><i class="material-icons left">exit_to_app</i>Log out</a></li>
-  </ul>
-  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons small">menu</i></a>
 
 
         <main>
--- MaPro - Website/headerBackpage.php
+++ MaPro - Website/headerBackpage.php
@@ -9,16 +9,13 @@
             <a href="homepage.php" class="brand-logo whiteToBlueLight zoom" style="font-weight: bold"><img style="height: 80%; top: 50%;transform: translateY(+14%); " src="pictures/LogoFull.png"></a>
 
 
-            <!-- Component that calls the side-menu when the window is reduced -->
-            <a href="#" class="sidenav-trigger hide-on-large-only zoom whiteToBlueLight" data-target="mobile-links">
-                <i class="material-icons">menu</i>
-            </a>
-
             <!-- Links -->
             <ul class="right hide-on-med-and-down">
                 <li><a class="whiteToBlueLight smallZoom" href="account.php"><i class="material-icons left">account_circle</i>My Profile</a></li>
                 <li><a class="whiteToBlueLight smallZoom" href="projectNew.php"><i class="material-icons left">add_to_photos</i>New Project</a></li>
                 <li><a class="whiteToBlueLight smallZoom" href="account.php"><i class="material-icons left">delete_sweep</i>Delete Project</a></li>
+                <li><a class="whiteToBlueLight smallZoom" href="employees.php"><i class="material-icons left">face</i>Employees</a></li>
+				<li><a class="whiteToBlueLight smallZoom" href="teams.php"><i class="material-icons left">groups</i>teams</a></li>
                 <li>
                     <form>
                         <div class="input-field smallZoom">
@@ -35,27 +32,36 @@
     </nav>
 </div>
 
-<ul id="mobile-links" class="sidenav">
-    <li><div class="user-view">
-      
-      <img class="circle" src="pictures/logo.png">
-      <span class="blueDeep name"> <?php echo $_SESSION['nom'] ?> </span>
 
-      <span class="blueLight email"><?php echo $_SESSION['role'] ?> </span>
-    </div></li>
+
+
+<ul id="slide-out" class="sidenav">
+    <li>
+        <div class="user-view">      
+            <img class="circle" src="pictures/logo.png">
+            <span class="blueDeep name"> <?php echo getEmployeeValue($_SESSION["ID"], "name"); ?> </span>
+            <span class="blueLight email"><?php echo getEmployeeValue($_SESSION["ID"], "type"); ?> </span>
+        </div>
+    </li>
     <li><a class="blueLightToBlueDeep" href="account.php"><i class="material-icons left">account_circle</i>My Profile</a></li>
-                <li><a class="blueLightToBlueDeep" href="projectNew.php"><i class="material-icons left">add_to_photos</i>New Project</a></li>
-                <li><a class="blueLightToBlueDeep" href="account.php"><i class="material-icons left">delete_sweep</i>Delete Project</a></li>
-                <li>
-                    <form>
-                        <div class="input-field">
-                            <input id="search" type="search" required>
-                            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-                            <i class="material-icons">close</i>
-                        </div>
-                    </form>
-                </li>
+    <li><a class="blueLightToBlueDeep" href="projectNew.php"><i class="material-icons left">add_to_photos</i>New Project</a></li>
+    <li><a class="blueLightToBlueDeep" href="account.php"><i class="material-icons left">delete_sweep</i>Delete Project</a></li>
+    <li><a class="blueLightToBlueDeep" href="employees.php"><i class="material-icons left">face</i>Employees</a></li>
+    <li><a class="blueLightToBlueDeep" href="teams.php"><i class="material-icons left">groups</i>teams</a></li>
+    <li>
+        <form>
+            <div class="input-field">
+                <input id="search" type="search" required>
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+            </div>
+        </form>
+    </li>
+    <li><a class=" btn waves-light red lighten-1" href='disconnect.php'><i class="material-icons left">exit_to_app</i>Log out</a></li>
   </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons small">menu</i></a>
+
+
 
 
 <!-- Compiled and minified JavaScript -->
