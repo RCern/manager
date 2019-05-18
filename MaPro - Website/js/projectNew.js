01ab1fa3e0234cbe68baf9317ddf728187a90136
opsilonn <hugues.begeot@efrei.net>
2019-05-18 17:47:03 +0200

    renamed files : projectNew
    now, all files are unified
    - projectNew.php
    - projectNew.js
    - BDD_projectNew.php

---
+++ MaPro - Website/js/projectNew.js
@@ -0,0 +1,24 @@
+$(document).ready(function()
+{
+	$("#create").click(function()
+	{
+		var name = $("#name_prefix").val();
+		var priority = $("#priority_prefix").val();
+		var team = $("#team").val();
+		var date = $("#date").val();
+		var hours = $("#hour_prefix").val();
+		var cost = $("#cost_prefix").val();
+		var revenues = $("#revenues_prefix").val();
+
+		// Checking for blank fields.
+		/*if( name =='' || priority =='' || team =='' || date =='' || hours =='' || cost =='' || revenues ==''){
+		alert("Please fill all fields...!!!!!!");
+		}else {*/
+
+		$.post("BDD_projectNew.php",{ name1: name, priority1: priority, team1: team, date1 : date, hours1: hours, cost1: cost, revenues1: revenues},
+		function(data)
+		{
+			alert("Done.");
+		});
+	});
+});
--- MaPro - Website/projectNew.php
+++ MaPro - Website/projectNew.php
@@ -40,7 +40,7 @@
         <!-- Add icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script type="text/javascript" src="js/newproject.js"></script>
+        <script type="text/javascript" src="js/projectNew.js"></script>
     </head>
 
 
